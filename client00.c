64635ddc06008462d09ab262c07a2afd62aaacbd
jv813yh <56434164+jv813yh@users.noreply.github.com>
2022-04-03 23:35:24 +0200

    Update client00.c

--- client00.c
+++ client00.c
@@ -52,7 +52,7 @@
 #include "salt_example_rs232.h" 
 /* /dev/ttyS0 (COM1 on windows) port */
 #define CPORT_NR                0
-/* 9600 baud, bit rate */
+/* 115200 baud, bit rate */
 #define B_TRATE                 115200
 
 /* ===== Salt-channel local macro definition & libraries ===== */
